5ae476407b8c6e962a9547c7c592cc68e5e9a12b
Tolga D <36132831+tolgademiirel@users.noreply.github.com>
2026-05-18 16:21:14 +0300

    Hide WPF scrollbar chrome globally

--- src/MusicShell.Wpf/App.xaml.cs
+++ src/MusicShell.Wpf/App.xaml.cs
@@ -1,6 +1,9 @@
 using System;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Media;
 using MusicShell.Infrastructure;
 
 namespace MusicShell;
@@ -10,6 +13,7 @@ public partial class App
     protected override void OnStartup(StartupEventArgs e)
     {
         base.OnStartup(e);
+        ApplyHiddenScrollerChrome();
 
         AppLogger.CleanupOldLogs(keepDays: 30);
         AppLogger.Log("ToolBridge başlatıldı.");
@@ -42,6 +46,43 @@ public partial class App
             AppLogger.LogException("Arka plan görevinde yakalanmayan hata", args.Exception);
             args.SetObserved();
         };
+    }
+
+    private void ApplyHiddenScrollerChrome()
+    {
+        try
+        {
+            Resources[typeof(ScrollBar)] = CreateHiddenScrollBarStyle();
+            AppLogger.Log("ScrollBar görsel bileşenleri gizlendi. Mouse wheel / touchpad scroll aktif kalacak.");
+        }
+        catch (Exception ex)
+        {
+            AppLogger.LogException("ScrollBar gizleme stili uygulanamadı", ex);
+        }
+    }
+
+    private static Style CreateHiddenScrollBarStyle()
+    {
+        var style = new Style(typeof(ScrollBar));
+        style.Setters.Add(new Setter(FrameworkElement.WidthProperty, 0d));
+        style.Setters.Add(new Setter(FrameworkElement.MinWidthProperty, 0d));
+        style.Setters.Add(new Setter(FrameworkElement.HeightProperty, 0d));
+        style.Setters.Add(new Setter(FrameworkElement.MinHeightProperty, 0d));
+        style.Setters.Add(new Setter(UIElement.OpacityProperty, 0d));
+        style.Setters.Add(new Setter(UIElement.IsHitTestVisibleProperty, false));
+        style.Setters.Add(new Setter(Control.FocusableProperty, false));
+        style.Setters.Add(new Setter(Control.BackgroundProperty, Brushes.Transparent));
+        style.Setters.Add(new Setter(Control.BorderThicknessProperty, new Thickness(0)));
+
+        var hiddenTemplate = new ControlTemplate(typeof(ScrollBar));
+        var hiddenRoot = new FrameworkElementFactory(typeof(Border));
+        hiddenRoot.SetValue(FrameworkElement.WidthProperty, 0d);
+        hiddenRoot.SetValue(FrameworkElement.HeightProperty, 0d);
+        hiddenRoot.SetValue(Border.BackgroundProperty, Brushes.Transparent);
+        hiddenTemplate.VisualTree = hiddenRoot;
+        style.Setters.Add(new Setter(Control.TemplateProperty, hiddenTemplate));
+
+        return style;
     }
 
     protected override void OnExit(ExitEventArgs e)
